5c770d1a4ba604408c1e3b3a05c84e089659306c
jay <jay_hqjhuang@tutorabc.com.cn>
2019-10-29 14:14:02 +0800

    Initial commit

---
+++ Example/TestLib/LWViewController.h
@@ -0,0 +1,13 @@
+//
+//  LWViewController.h
+//  TestLib
+//
+//  Created by MountainClimbers on 10/29/2019.
+//  Copyright (c) 2019 MountainClimbers. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LWViewController : UIViewController
+
+@end
---
+++ Example/TestLib/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestLib
+//
+//  Created by MountainClimbers on 10/29/2019.
+//  Copyright (c) 2019 MountainClimbers. All rights reserved.
+//
+
+@import UIKit;
+#import "LWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LWAppDelegate class]));
+    }
+}
