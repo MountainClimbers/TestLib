74f3e5a4f1a888ff6a97231b4b8a6b2f9c7d00c6
jayHuang <qjh021@yeah.net>
2019-11-16 16:24:56 +0800

    commit2

--- Example/TestLib/LWAppDelegate.m
+++ Example/TestLib/LWAppDelegate.m
@@ -26,6 +26,8 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    // master分支修改了commit2
+
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
